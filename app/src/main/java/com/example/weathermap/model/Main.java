3632761b4ef1a9a875ee32084582fbe08d8efc6c
MogaAlex <66469158+MogaAlex@users.noreply.github.com>
2020-06-05 14:47:12 +0300

    first commit

---
+++ app/src/main/java/com/example/weathermap/model/Main.java
@@ -0,0 +1,12 @@
+package com.example.weathermap.model;
+
+public class Main {
+    private float temp;
+
+    public float getTemp() {
+        return temp;
+    }
+    public void setTemp(final float temp){
+        this.temp = temp;
+    }
+}
